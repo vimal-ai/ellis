3da511721eda7aef050e931c60dcac062f6761ce
vimal mishra <mivimal786@gmail.com>
2020-11-17 16:07:58 +0530

    fix lag issue while using mic

--- chatbot.py
+++ chatbot.py
@@ -83,13 +83,18 @@ get input from mic
 def getAudio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        audio = r.listen(source)
+        print('0')
+        audio = r.listen(source, timeout=2, phrase_time_limit=5)
+        print('1')
         said = ""
         try:
+            print('2')
             said = r.recognize_google(audio)
+            print('3')
+            return said
         except Exception as e:
             return str(e)
-    return said
+
 """
 for automation in computer
 """
